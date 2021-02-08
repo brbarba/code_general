5c0fd2bc7a2530099906ad69c5d412e7566c3faa
bruno barba <bruno.barba@gmail.com>
2021-02-08 17:47:28 -0600

    carroect var error

--- env_var.py
+++ env_var.py
@@ -2,10 +2,10 @@ import os
 
 EMAIL_ADDRESS = os.environ['EMAIL_USER']
 EMAIL_PASSWORD = os.environ['EMAIL_PASS']
-#access_token = os.environ['WEBEX_TEAMS_TOKEN']
-#room_ID = os.environ['NOTIFY_ROOM_ID']
+access_token = os.environ['WEBEX_TEAMS_TOKEN']
+room_ID = os.environ['NOTIFY_ROOM_ID']
 
-print('EMAIL_USER')
-print('EMAIL_PASS')
-#print(WEBEX_TEAMS_TOKEN)
-#print(NOTIFY_ROOM_ID)
+print(EMAIL_ADDRESS)
+print(EMAIL_PASSWORD)
+print(access_token)
+print(room_ID)
