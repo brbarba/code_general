7f34e1fc57f747a42b7b414b448b7384f9377d4b
bruno barba <bruno.barba@gmail.com>
2021-02-08 17:27:01 -0600

    adding var

--- env_var.py
+++ env_var.py
@@ -4,6 +4,8 @@ import requests
 
 EMAIL_ADDRESS = os.environ['EMAIL_USER']
 EMAIL_PASSWORD = os.environ.get('EMAIL_PASS')
+access_token = os.environ.get('WEBEX_TEAMS_TOKEN')
+room_ID = os.environ.get('NOTIFY_ROOM_ID')
 
-print(EMAIL_ADDRESS)
-print(EMAIL_PASSWORD)
+print(WEBEX_TEAMS_TOKEN)
+print(NOTIFY_ROOM_ID)
