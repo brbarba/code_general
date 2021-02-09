558dfcfc605fce940959f05cdb0b122cb91f2702
bruno barba <bruno.barba@gmail.com>
2021-02-08 18:07:50 -0600

    sync var

--- env_var.py
+++ env_var.py
@@ -1,11 +1,11 @@
 import os
 
-EMAIL_ADDRESS = os.environ['WEBEX_TEAMS_TOKEN']
+EMAIL_ADDRESS = os.environ['EMAIL_USER']
 EMAIL_PASSWORD = os.environ['EMAIL_PASS']
-#access_token = os.environ['WEBEX_TEAMS_TOKEN']
-#room_ID = os.environ.get('NOTIFY_ROOM_ID')
+access_token = os.environ['WEBEX_TEAMS_TOKEN']
+room_ID = os.environ.get('NOTIFY_ROOM_ID')
 
 print(EMAIL_ADDRESS)
 print(EMAIL_PASSWORD)
-#print(access_token)
-#print(room_ID)
+print(access_token)
+print(room_ID)
