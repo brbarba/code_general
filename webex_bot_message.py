f50e009b24ac54ba8959fc01763f5f840cfd9b75
Bruno Barba <bruno.barba@gmail.com>
2021-02-08 19:15:27 -0600

    bot message

---
+++ webex_bot_message.py
@@ -0,0 +1,36 @@
+
+import requests
+import os
+# documentation at https://developer.webex.com/docs/api/v1/messages/create-a-message
+
+apiUrl = 'https://webexapis.com/v1/messages'
+access_token = os.environ['WEBEX_TEAMS_TOKEN']
+room_ID = os.environ['NOTIFY_ROOM_ID']
+bot_token = os.environ['CHTRPM_NOTI_BOT_TOKEN']
+email_ID = 'chtrpm_noti@webex.bot'
+
+
+httpHeaders = { 'Content-type': 'application/json', 'Authorization': 'Bearer ' + bot_token }
+
+#body = { 'roomId': room_ID, 'text': 'Test notification 4\nusando room_ID como variable\nFrom Pyhton' }
+body = { 'toPersonEmail': email_ID, 'text': 'Bot Message from Bot' }
+
+response = requests.post( url = apiUrl, json = body, headers = httpHeaders )
+
+print( response.status_code )
+print( response.text )
+
+messages = [
+    'as an example of markdown',
+    '**Warning!!!**',
+    '_Warning!!!_',
+    '[Danger, Will Robinson!!!](https://en.wikipedia.org/wiki/Lost_in_Space#Catchphrases)'
+    ]
+
+for message in messages:
+
+    body = { 'roomId': room_ID, 'markdown': message }
+    response = requests.post( url = apiUrl, json = body, headers = httpHeaders )
+
+    print( response.status_code )
+    print( response.text )
