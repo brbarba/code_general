ae10a316632f2e9f3edb6ed55091fdf24c45bdb9
Bruno Barba <bruno.barba@gmail.com>
2021-02-08 19:17:20 -0600

    adding bot token var

--- env_var.py
+++ env_var.py
@@ -4,8 +4,10 @@ EMAIL_ADDRESS = os.environ['EMAIL_USER']
 EMAIL_PASSWORD = os.environ['EMAIL_PASS']
 access_token = os.environ['WEBEX_TEAMS_TOKEN']
 room_ID = os.environ.get('NOTIFY_ROOM_ID')
+bot_token = os.environ['CHTRPM_NOTI_BOT_TOKEN']
 
 print(EMAIL_ADDRESS)
 print(EMAIL_PASSWORD)
 print(access_token)
 print(room_ID)
+print(bot_token)
