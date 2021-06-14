4c184239ce8c09499cd754e7a7ff682f5c0d141e
brbarba <bruno.barba@gmail.com>
2021-06-14 17:45:59 -0500

    shelly env virable

---
+++ Shelly/Shelly_api.py
@@ -0,0 +1,20 @@
+import requests
+import os
+
+SHELLY_HOME_Token = os.environ.get('SHELLY_TOKEN')
+Device_ID = os.environ.get('Alacena_Shelly_id')
+
+url = "https://shelly-3-eu.shelly.cloud/device/relay/control/"
+
+payload={'auth_key': ,SHELLY_HOME_Token
+'turn': 'on',
+'channel': '0',
+'id': Device_ID}
+files=[
+
+]
+headers = {}
+
+response = requests.request("POST", url, headers=headers, data=payload, files=files)
+
+print(response.text)
