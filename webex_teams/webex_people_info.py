8386d39b3ffebb02cd58cf2b6b8392bb4e7bb2f2
Bruno Barba <bruno.barba@gmail.com>
2021-06-18 19:54:55 -0500

    correcting

--- webex_teams/webex_people_info.py
+++ webex_teams/webex_people_info.py
@@ -1,12 +1,13 @@
 import requests
 import os
 BOT_TOKEN = os.environ.get('People_ready_bot_TOKEN')
-userid = brbarba
-url = "https://webexapis.com/v1/people" + userid + "%40cisco.com"
+userid = 'brbarba'
+url = "https://webexapis.com/v1/people?email=" + userid + "%40cisco.com"
 payload={}
 headers = {
   'Authorization': 'Bearer ' + BOT_TOKEN }
 
-#response = requests.request("GET", url, headers=headers, data=payload)
 print(url)
-#print(response.text)
+response = requests.request("GET", url, headers=headers, data=payload)
+
+print(response.text)
