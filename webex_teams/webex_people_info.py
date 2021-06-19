ff589b0ff446bd6e9beb6676d268265ad142b625
brbarba <bruno.barba@gmail.com>
2021-06-18 19:51:35 -0500

    input userid

--- webex_teams/webex_people_info.py
+++ webex_teams/webex_people_info.py
@@ -2,8 +2,8 @@ import requests
 import os
 import json
 BOT_TOKEN = os.environ.get('People_ready_bot_TOKEN')
-userid = 'brbarba'
-url = "https://webexapis.com/v1/people?email=" + userid + "%40cisco.com"
+userid = input("Enter the userID to check status: ")
+url = "https://webexapis.com/v1/people?email=" + userid + "@cisco.com"
 payload={}
 headers = {
   'Authorization': 'Bearer ' + BOT_TOKEN }
@@ -13,9 +13,10 @@ response = requests.request("GET", url, headers=headers, data=payload)
 json_data = json.loads(response.text)
 name = response.json()["items"][0]["displayName"]
 status = response.json()["items"][0]["status"]
+message = name + ' tiene como status ' + status
 print('\n')
-print(response.text)
+#print(response.text)
 print('\n')
-print(json.dumps(json_data, indent = 4, sort_keys=True))
+#print(json.dumps(json_data, indent = 4, sort_keys=True))
 print('\n')
 print(name + ' tiene como status ' + status)
