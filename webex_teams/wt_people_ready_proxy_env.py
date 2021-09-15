4948f7c02b05f56cb92d9dedf36f479e31a8fe2e
brbarba <bruno.barba@gmail.com>
2021-09-14 20:49:37 -0500

    people ready with proxy

---
+++ webex_teams/wt_people_ready_proxy_env.py
@@ -0,0 +1,58 @@
+import requests
+import os
+import json
+import time
+from dotenv import load_dotenv
+
+load_dotenv()
+
+BOT_TOKEN = os.getenv('People_ready_bot_TOKEN')
+userid = input("Enter the userID to check status: ")
+
+url = "https://webexapis.com/v1/people?email=" + userid + "@cisco.com"
+#variables to send message
+ToEmail = 'brbarba@cisco.com'
+
+proxies = {'https': 'https://proxy.esl.cisco.com:80'}
+apiUrl = 'https://webexapis.com/v1/messages'
+access_token = 'replace with your_access_token'
+room_ID = 'replace with your room ID'
+email_ID = 'gifbot@webex.bot'
+httpHeaders = { 'Content-type': 'application/json', 'Authorization': 'Bearer ' + BOT_TOKEN }
+
+payload={}
+headers = {
+  'Authorization': 'Bearer ' + BOT_TOKEN }
+
+text_in1 = ' tiene como status\n'
+text_in2 = ' ya esta disponible con status\n'
+
+def get_status():
+    global name
+    global status
+    response = requests.request("GET", url, proxies=proxies, headers=headers, data=payload)
+    json_data = json.loads(response.text)
+    name = response.json()["items"][0]["displayName"]
+    status = response.json()["items"][0]["status"]
+
+def post_message(status_post, text_inside):
+    message = name + text_inside + status_post
+    body = { 'toPersonEmail': ToEmail, 'text': message }
+    response_post = requests.post( url = apiUrl, proxies = proxies, json = body, headers = httpHeaders )
+
+#    print('\n')
+#    print(message)
+#    print( response_post.status_code )
+
+print(url)
+
+get_status()
+
+post_message(status, text_in1)
+
+while status != 'active':
+    get_status()
+    time.sleep(60)
+    print('polling user')
+else:
+    post_message(status, text_in2)
